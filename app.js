8daa421d5e6808998b1510be0e58275751b0735a
HajerBA <hajerbnch@gmail.com>
2018-01-10 14:39:47 +0100

    commit

---
+++ app.js
@@ -0,0 +1,12 @@
+class BabyPatient {
+
+    constructor(name,surname,age,weight,state) {
+        this.name=name;
+        this.surname=surname;
+        this.age=age;
+        this.weight=weight;
+        this.state=state;
+        
+
+    }
+}
