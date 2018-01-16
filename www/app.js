5b192e8f3808f8bd8abe0b266edb302449dfe154
HajerBA <hajerbnch@gmail.com>
2018-01-16 17:33:27 +0100

    updates

--- www/app.js
+++ www/app.js
@@ -1,3 +1,18 @@
+//use strict
+let imgexit=document.querySelector("#imgexit");
+
+imgexit.addEventListener('click',function fermerFenetreCourante()
+{
+  window.close();
+});
+
+let imginst=document.querySelector("#instruction");
+
+imginst.addEventListener('click',function fermerFenetreCourante()
+{
+ alert();//msgbox
+});
+
 //pour les interactions
 let newDoc=new Doctor();
 let newBbPatient= new BabyPatient();
